2bd243ba593d08457f53386a49737aa5ddd9c807
Danylo Lopatnov <danlopatnov@gmail.com>
2024-04-04 15:02:23 -0400

    added project details

--- hw2/src/Pages/Projects.js
+++ hw2/src/Pages/Projects.js
@@ -5,21 +5,21 @@ const Projects = () => {
   const projects = [
     {
       title: 'Mobile Fitness App',
-      description: 'Developed a full-stack mobile application using the MAUI .NET framework connected to a remote Node.js server with a MySQL database through Express.js. Used SQL Management Studio to set up the database. \n\nLed a team to effectively design and integrate app features within a structured timeline using GitHub and ClickUp. \n\nIntegrated REST APIs to enable barcode database access, google login and barcode recognition from a live camera.Deserialized JSON data into the structured class system of the application.',
-      images: ['image1.png', 'image2.png', 'image3.png'],
-      technologies: 'C#, .NET, Node.js, REST API, MySQL, Xamarin, MUAI'
+      description: 'Developed a full-stack mobile application using the MAUI .NET framework connected to a remote Node.js server with a MySQL database through Express.js. Used SQL Management Studio to set up the database. \n\nLed a team to effectively design and integrate app features within a structured timeline using GitHub and ClickUp. \n\nIntegrated REST APIs to enable barcode database access, google login and barcode recognition from a live camera. Deserialized JSON data into the MVVM structured class system of the application.',
+      images: ['image1.png', 'image3.png', 'image2.png'],
+      technologies: 'C#, .NET, Node.js, REST API, MySQL, Xamarin, MUAI, MVVM'
     },
     {
       title: 'Rocket Propulsion Analysis',
-      description: 'Created a Rocket Propulsion Simulation Analysis software using C++ to take data on rocket flight path parameters from a UI and perform physics calculations to predict velocity and mass at any given time. I used standard programming practices to structure the application to allow for modulation and scalability.',
+      description: 'Created a Rocket Propulsion Simulation Analysis software using C++ to take data on rocket flight path parameters from a UI and perform physics calculations to predict velocity and mass at any given time. \n\nI used standard programming practices to structure the application to allow for modulation and scalability.',
       images: ['image4.png', 'image6.png', 'image5.png'],
       technologies: 'Windows.h, iomanip, conio.h, lists, iterators'
     },
     {
       title: 'Click Bots',
-      description: 'Developed “Click-Bots” with C++ and Windows.h to simulate adaptable human input by manipulating keyboard andmouse movement. This was used to make appointment reservations by automatically constantly checking for earlier times and sending a mobile notification when it was available. This application saved a year of waiting time \n\nUtilized Windows.h, conio.h, cmath, iomanip libraries for calculation time optimisation and extra functionality.',
-      images: ['image7.jpg', 'image8.jpg', 'image9.jpg'],
-      technologies: 'OpenCV, '
+      description: 'Developed “Click-Bots” with C++ and Windows.h to simulate adaptable human input by manipulating keyboard and mouse movement. This was used to make appointment reservations by automatically checking for earlier times and sending a mobile notification when it was available. This application saved a year of waiting time. \n\nMade click bots in Python with image recognition to automate user input with dynamically changing websites, significantly reducing bugs and mis-clicking.',
+      images: ['image7.png', 'image8.png', 'image9.png'],
+      technologies: 'OpenCV, Python, Numpy, Windows.h, stdlib.h, vectors'
     }
   ];
 
